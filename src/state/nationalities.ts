7367ccffc8ec1189ad34e6aa0b222a7793562ad0
Nuno David <email@ndavd.com>
2022-05-01 23:15:38 +0100

    state: specified nationalities type

--- src/state/nationalities.ts
+++ src/state/nationalities.ts
@@ -1,7 +1,7 @@
 import { atom } from 'recoil';
 import { countries } from '../utils';
 
-export const nationalitiesState = atom({
+export const nationalitiesState = atom<string[]>({
   key: "nationalities",
   default: countries.map(({code}) => code),
 });
