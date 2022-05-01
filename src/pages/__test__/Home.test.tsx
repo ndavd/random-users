58b3dee4b47644e77dc92eb54994360d518f6e1b
Nuno David <email@ndavd.com>
2022-05-01 22:26:32 +0100

    testing: fix and refactor Home.test.tsx

--- src/pages/__test__/Home.test.tsx
+++ src/pages/__test__/Home.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,7 +10,7 @@ import UserList from '../../components/UserList';
 import { results } from '../../api';
 import { countries } from '../../utils';
 
-const renderUsers = async (): Promise<HTMLLIElement[]> => {
+const renderApp = async () => {
   const queryClient = new QueryClient();
   render(
     <RecoilRoot>
@@ -24,19 +24,19 @@ const renderUsers = async (): Promise<HTMLLIElement[]> => {
       </QueryClientProvider>
     </RecoilRoot>
   );
-
-  return await screen.findAllByTestId("user");
 }
 
 describe("Home", () => {
 
   it("UserList fetches and displays the correct number of users", async () => {
-    const users = await renderUsers();
+    await renderApp();
+    const users = await screen.findAllByTestId("user");
+
     expect(users.length).toBe(results);
   });
 
   it("NationalityFilter re-fetches users accordingly", async () => {
-    await renderUsers();
+    await renderApp();
 
     const nationalityCheckboxes = screen.getAllByRole("checkbox");
 
@@ -53,18 +53,22 @@ describe("Home", () => {
   });
 
   it("NameFilter filters users", async () => {
-    await renderUsers();
+    await renderApp();
 
     const input = screen.getByRole("textbox");
     fireEvent.change(input, { target: { value: "alb" } });
 
-    const users = await screen.findAllByTestId("user");
+    // Using queryAllBy because `users` might be empty, so it would return an
+    // error if using findAllBy
+    await waitFor(() => {
+      const users = screen.queryAllByTestId("user");
+      expect(users.length).toBeLessThan(results);
+    });
 
-    expect(users.length).toBeLessThan(results);
   });
 
   it("UserCounter displays the number of users", async () => {
-    await renderUsers();
+    await renderApp();
 
     const counter = await screen.findByText(new RegExp(`${results} results`));
 
